88ef3f5aa7c3509b2a47218e9090dbb5410c1ca1
Jiplonk <100437797+Jiplonk@users.noreply.github.com>
2022-03-10 10:09:46 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,14 +1,14 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `››╭─〘 *Donasi* 〙
 ╭╡
-│┝‷✧ *Pulsa:* 6285712420674
-│┝‷✧ *Dana:* 6285712420674
-│┝‷✧ *Linkaja:* 6285712420674
-│┝‷✧ *Gopay:* 6285712420674
-│┝‷✧ *Ovo:* 6285712420674
+│┝‷✧ *Pulsa:* 6283113989278
+│┝‷✧ *Dana:* 082323341102
+│┝‷✧ *Linkaja:* 628
+│┝‷✧ *Gopay:* 628
+│┝‷✧ *Ovo:* 6283113989278
 │╰───···─────
 │⁺◛˖ Ingin Donasi? Chat
-│wa.me/6285712420674?text=kak+mau+donasi
+│wa.me/6283113989278?text=kak+mau+donasi
 ╰──────────···───╮
 ▌│█║▌║▌║║▌║▌║█│▌
 `.trim(), `${wm}`, 'Owner', '.owner')
